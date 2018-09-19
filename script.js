65e630832bbe7c484e83be31ef59f8e6e8e23849
josearmando23 <42612613+josearmando23@users.noreply.github.com>
2018-09-19 10:55:21 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+let binario = numero => {
+	let binarioArray = []
+	while(numero > 1){
+		binarioArray.push(numero % 2)
+		numero = Math.floor(numero / 2)
+		
+	}
+	
+	binarioArray.push(numero)
+	return parseInt(binarioArray.reverse().join(""))
+	
+	
+}
+
+console.log(binario(10));
